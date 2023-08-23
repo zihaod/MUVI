08d5d982aeedd4418d9003cc120eab434f454bb5
yxm46-txk47 <ma_nicolaus@163.com>
2023-08-24 00:56:22 +0100

    delete llama in the model to simplify

--- muvi/models/muvi_model.py
+++ muvi/models/muvi_model.py
@@ -50,28 +50,28 @@ class MUVI(BaseModel):
         print('Loading Audio Encoder Done')
 
         print('Loading LLAMA')
-        self.llama_tokenizer = LlamaTokenizer.from_pretrained(llama_model, use_fast=False)
-        self.llama_tokenizer.pad_token = self.llama_tokenizer.eos_token
+        # self.llama_tokenizer = LlamaTokenizer.from_pretrained(llama_model, use_fast=False)
+        # self.llama_tokenizer.pad_token = self.llama_tokenizer.eos_token
 
-        if self.low_resource:
-            self.llama_model = LlamaForCausalLM.from_pretrained(
-                llama_model,
-                torch_dtype=torch.float16,
-                load_in_8bit=True,
-                device_map={'': device_8bit}
-            )
-        else:
-            self.llama_model = LlamaForCausalLM.from_pretrained(
-                llama_model,
-                torch_dtype=torch.float16,
-            )
+        # if self.low_resource:
+        #     self.llama_model = LlamaForCausalLM.from_pretrained(
+        #         llama_model,
+        #         torch_dtype=torch.float16,
+        #         load_in_8bit=True,
+        #         device_map={'': device_8bit}
+        #     )
+        # else:
+        #     self.llama_model = LlamaForCausalLM.from_pretrained(
+        #         llama_model,
+        #         torch_dtype=torch.float16,
+        #     )
 
-        for name, param in self.llama_model.named_parameters():
-            param.requires_grad = False
-        print('Loading LLAMA Done')
+        # for name, param in self.llama_model.named_parameters():
+        #     param.requires_grad = False
+        # print('Loading LLAMA Done')
 
         self.llama_proj = nn.Linear(
-            self.audio_encoder.config.hidden_size, self.llama_model.config.hidden_size
+            self.audio_encoder.config.hidden_size, 4096
         )
         self.max_txt_len = max_txt_len
         self.end_sym = end_sym
