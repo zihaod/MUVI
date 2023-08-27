de846f5ea7e8540ac083dfb1ea8563fae48f8008
zihaod <65971377+zihaod@users.noreply.github.com>
2023-08-27 11:58:12 -0400

    Update audio_text_pair_builder.py

--- muvi/datasets/builders/audio_text_pair_builder.py
+++ muvi/datasets/builders/audio_text_pair_builder.py
@@ -45,3 +45,42 @@ class MusicCapsBuilder(BaseDatasetBuilder):
  
 
         return datasets
+
+
+
+@registry.register_builder("musicqa")
+class MusicQABuilder(BaseDatasetBuilder):
+    train_dataset_cls = MusicQADataset
+
+    DATASET_CONFIG_DICT = {
+        "default": "configs/datasets/musicqa/default.yaml",
+    }
+
+    def build_datasets(self):
+        # at this point, all the annotations and image/videos should be all downloaded to the specified locations.
+        logging.info("Building datasets...")
+        #self.build_processors()
+
+        #build_info = self.config.build_info
+        #storage_path = build_info.storage
+
+        datasets = dict()
+
+        #if not os.path.exists(storage_path):
+        #    warnings.warn("storage path {} does not exist.".format(storage_path))
+
+        #get processor
+        processor = Wav2Vec2FeatureExtractor.from_pretrained(self.config.processor, trust_remote_code=True)
+
+
+        # create datasets
+        dataset_cls = self.train_dataset_cls
+
+        datasets['train'] = dataset_cls(
+            processor=processor,
+            split=self.config.split, 
+            data_dir=self.config.data_dir, 
+        )
+ 
+
+        return datasets
