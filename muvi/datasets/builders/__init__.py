01367005fa1235b8cae36214f05d626723ae4e51
zihaod <65971377+zihaod@users.noreply.github.com>
2023-09-09 15:48:52 -0400

    Update __init__.py

--- muvi/datasets/builders/__init__.py
+++ muvi/datasets/builders/__init__.py
@@ -7,14 +7,15 @@
 
 from muvi.datasets.builders.base_dataset_builder import load_dataset_config
 from muvi.datasets.builders.audio_text_pair_builder import (
-    MusicCapsBuilder, MusicQABuilder, MSDBuilder
+    MusicCapsBuilder, MusicQABuilder, MSDBuilder, CMIBuilder
 )
 from muvi.common.registry import registry
 
 __all__ = [
     "MusicCapsBuilder",
     "MusicQABuilder",
-    "MSDBuilder"
+    "MSDBuilder",
+    "CMIBuilder"
 ]
 
 
