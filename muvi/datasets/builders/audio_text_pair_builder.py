f1070cccc329aaf98282cd5999fedc0c3254bb6c
zihaod <65971377+zihaod@users.noreply.github.com>
2023-09-09 15:48:30 -0400

    Update audio_text_pair_builder.py

--- muvi/datasets/builders/audio_text_pair_builder.py
+++ muvi/datasets/builders/audio_text_pair_builder.py
@@ -9,6 +9,7 @@ from muvi.datasets.builders.base_dataset_builder import BaseDatasetBuilder
 from muvi.datasets.datasets.musiccaps_dataset import MusicCapsDataset
 from muvi.datasets.datasets.musicqa_dataset import MusicQADataset
 from muvi.datasets.datasets.msd_dataset import MSDDataset
+from muvi.datasets.datasets.cmi_dataset import CMIDataset
 
 
 @registry.register_builder("musiccaps")
@@ -48,7 +49,42 @@ class MusicCapsBuilder(BaseDatasetBuilder):
 
         return datasets
 
+@registry.register_builder("cmi")
+class CMIBuilder(BaseDatasetBuilder):
+    train_dataset_cls = CMIDataset
 
+    DATASET_CONFIG_DICT = {
+        "default": "configs/datasets/cmi/default.yaml",
+    }
+
+    def build_datasets(self):
+        # at this point, all the annotations and image/videos should be all downloaded to the specified locations.
+        logging.info("Building datasets...")
+        #self.build_processors()
+
+        #build_info = self.config.build_info
+        #storage_path = build_info.storage
+
+        datasets = dict()
+
+        #if not os.path.exists(storage_path):
+        #    warnings.warn("storage path {} does not exist.".format(storage_path))
+
+        #get processor
+        processor = Wav2Vec2FeatureExtractor.from_pretrained(self.config.processor, trust_remote_code=True)
+
+
+        # create datasets
+        dataset_cls = self.train_dataset_cls
+
+        datasets['train'] = dataset_cls(
+            processor=processor,
+            split=self.config.split, 
+            data_dir=self.config.data_dir, 
+        )
+ 
+
+        return datasets
 
 @registry.register_builder("musicqa")
 class MusicQABuilder(BaseDatasetBuilder):
